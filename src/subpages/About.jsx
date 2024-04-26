e7b38f5c591e4bf9bf54f9206179de88e2f20909
Ali Vidler <alisonvidler@gmail.com>
2024-04-25 20:49:02 -0400

    Made changes to the about page

--- src/subpages/About.jsx
+++ src/subpages/About.jsx
@@ -1,11 +1,26 @@
 import React from 'react'
+import About from '../data/Rants'
 
-function About() {
+function about() {
   return (
     <div>
-      <h1>About</h1>
-      <body>Everyday my partner comes to ask me what I want to eat and no matter how many times I've told them they always ask. I always want the same things in the same order. This blog is dedicated to them.</body>
+      <div className="grid-cols-1 grid gap-2 bg-slate-900 pt-5">
+    {About.map((post) => (
+        <div key={post.id} className=" bg-slate-200 p-5 ml-20 mr-20 mt-1  rounded-lg  hover:bg-white relative" >
+            <h2 className="font-bold p-1 justify-center">{post.title}</h2>
+            <p className="font-normal p-1">{post.excerpt}</p>
+            <span className="bg-orange-500 absolute top-0 right-4 p-1 rounded-b-lg">5 Min Read</span>
+            
+
+
+    
+         </div>
+    ))}
+    
     </div>
+    </div>
+
+    
   )
 }
 
