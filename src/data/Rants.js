e0ec9f56bae1c8bcf70a128c21535549e09ae45f
Ali Vidler <alisonvidler@gmail.com>
2024-04-25 20:24:58 -0400

    Made changes to the blog

--- src/data/Rants.js
+++ src/data/Rants.js
@@ -31,27 +31,27 @@ const rants = [
     },
     {
       id: 3,
-      title: "Machine Learning Techniques for Data Analysis",
-      author: "David Brown",
+      title: "I hate entitled people with animals",
+      author: "Me",
       excerpt:
-        "Introduction to machine learning algorithms and their applications in data analysis.",
-      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend ante eu massa molestie, sed aliquet risus rutrum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Ut non est lectus. In vel eros nec turpis consequat cursus. Duis vestibulum vestibulum turpis, non lobortis nulla. Suspendisse potenti. Nulla facilisi. Fusce maximus justo eget nulla faucibus congue. Ut id magna sed nisl vehicula laoreet non a metus. Sed vulputate, mauris sit amet tincidunt laoreet, mauris justo volutpat ante, a posuere dolor nulla vel lorem. Duis ac posuere nulla, a semper sapien. Sed vestibulum sodales felis ut placerat. Quisque et libero bibendum, feugiat lectus et, efficitur lorem.",
+        "No one will love your animal the way you do.",
+      body: "The type of person who loves their dog and thinks everyone else feels the same way. They have a complete disregard for boundaries with their animal including with other animals. They are the type of person to see you walking your dog, look at theirs and go do you want to say hi? Then when I pull mine away they go it's okay, he's friendly. While i rely, it's okay, mine needs space. This person will hear you having a party and bring their dog without asking. They will just show up. They will talk at length about how everyone loves their dog, but as someone who walked dogs for years, people who love dogs will approach you to pet a dog.",
     },
     {
       id: 4,
-      title: "Introduction to Quantum Computing",
-      author: "Sophia Williams",
+      title: "I hate the idea of buying a house",
+      author: "Me",
       excerpt:
-        "An overview of quantum computing principles and their potential impact on computing.",
-      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend ante eu massa molestie, sed aliquet risus rutrum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Ut non est lectus. In vel eros nec turpis consequat cursus. Duis vestibulum vestibulum turpis, non lobortis nulla. Suspendisse potenti. Nulla facilisi. Fusce maximus justo eget nulla faucibus congue. Ut id magna sed nisl vehicula laoreet non a metus. Sed vulputate, mauris sit amet tincidunt laoreet, mauris justo volutpat ante, a posuere dolor nulla vel lorem. Duis ac posuere nulla, a semper sapien. Sed vestibulum sodales felis ut placerat. Quisque et libero bibendum, feugiat lectus et, efficitur lorem.",
+        "Buying a house doesn't always make financial sense.",
+      body: "Buying a house used to make sense in the past when they were a good investment, but depending on where you live, buying a house might be a horrible financial decision and more of a liability than anything. You might have a lot of your time used to upkeep it, mow the lawn, fix a broken pipe, and stress. Renting might not always be an investment but it's way less stressful to maintain. Renting might make a lot of sense for someone with anxiety who doesn't want to worry about constantly fixing things.",
     },
     {
       id: 5,
-      title: "The Evolution of Cybersecurity",
-      author: "Michael Johnson",
+      title: "I hate travel shamers",
+      author: "Me",
       excerpt:
-        "Exploring the history and advancements in cybersecurity technologies.",
-      body: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed eleifend ante eu massa molestie, sed aliquet risus rutrum. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Ut non est lectus. In vel eros nec turpis consequat cursus. Duis vestibulum vestibulum turpis, non lobortis nulla. Suspendisse potenti. Nulla facilisi. Fusce maximus justo eget nulla faucibus congue. Ut id magna sed nisl vehicula laoreet non a metus. Sed vulputate, mauris sit amet tincidunt laoreet, mauris justo volutpat ante, a posuere dolor nulla vel lorem. Duis ac posuere nulla, a semper sapien. Sed vestibulum sodales felis ut placerat. Quisque et libero bibendum, feugiat lectus et, efficitur lorem.",
+        "Travel shamers are upset that you don't have their same values.",
+      body: "Why travel shame someone? Is it because you'd like to travel but can't? Is it a financial reason or do you just not want to make the effort to plan a trip so it's easier to shame others?.",
     },
     {
       id: 6,
